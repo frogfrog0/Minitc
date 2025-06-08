87f8acd2b3ffd1e7e941e4acb117edd10b703bba
frogfrog0 <164091334+frogfrog0@users.noreply.github.com>
2025-06-08 18:47:53 +0200

    Add files via upload

---
+++ EFcore test/Models/Kurs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EFcore_test.Models
+{
+    public class Kurs
+    {
+        public int Id { get; set; }                    // Klucz główny
+
+        [Required]
+        [StringLength(10)]
+        public string Nazwa { get; set; } = string.Empty;
+
+        // Relacja M:N ze Studentami
+        public List<Student> Studenci { get; set; } = new List<Student>();
+    }
+}
+
---
+++ EFcore test/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcore_test.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }                    // Klucz główny (automatyczny)
+
+        [Required]
+        public int IdStudenta { get; set; }            // Unikalny numer studenta (1, 2, 3...)
+
+        // Relacja 1:1 z Profile
+        public StudentProfile Profile { get; set; } = null!;
+
+        // Relacja M:N z Kursami
+        public List<Kurs> Kursy { get; set; } = new List<Kurs>();
+    }
+}
